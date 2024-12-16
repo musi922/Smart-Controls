715328fecce8738760c591502568238a1630bf67
Richard Musime <richardmusime6@gmail.com>
2024-12-16 19:11:15 +0200

    feat: added search field

--- webapp/controller/Orders.controller.js
+++ webapp/controller/Orders.controller.js
@@ -3,11 +3,11 @@ sap.ui.define([
     "sap/ui/model/odata/ODataModel",
     "sap/ui/model/Filter",
     "sap/ui/model/FilterOperator",
-    "sap/ui/model/FilterType"
-], function (Controller, ODataModel, Filter, FilterOperator, FilterType) {
+    "sap/ui/model/FilterType",
+    "sap/m/MessageBox"
+], function (Controller, ODataModel, Filter, FilterOperator, FilterType,MessageBox) {
     "use strict";
     return Controller.extend("lazyloading.controller.Orders", {
-
         onInit: function () {
             var oModel = new ODataModel("http://localhost:5000/odata", {
                 defaultBindingMode: "TwoWay",
@@ -21,6 +21,55 @@ sap.ui.define([
             let oFilter = new Filter("OrderID", FilterOperator.EQ, orderID,false, FilterType.Application);
             this.getView().byId("orderTable").getBinding("items").filter(oFilter);
 
+        },
+        onSearchOrders: function (oEvent) {
+            let query = oEvent.getParameter("query");
+            let list = this.byId("orderTable");
+            let binding = list.getBinding("items");
+            
+            let aFilters = [];
+            
+            if (query) {
+                let queryNumber = parseInt(query);
+                
+                if (!isNaN(queryNumber)) {
+                    aFilters.push(
+                        new Filter({
+                            filters: [
+                                new Filter("OrderID", FilterOperator.EQ, queryNumber),
+                                new Filter("ProductID", FilterOperator.EQ, queryNumber),
+                                new Filter("UnitPrice", FilterOperator.EQ, queryNumber),
+                                new Filter("Quantity", FilterOperator.EQ, queryNumber),
+                                new Filter("Discount", FilterOperator.EQ, queryNumber)
+                            ],
+                            and: false
+                        })
+                    );
+                }
+            }
+            binding.filter(aFilters);
+        }
+        
+        , 
+        onDelete: function (oEvent) {
+            var oItem = oEvent.getSource().getParent().getBindingContext();
+            var oData = oItem.getObject();
+            var oModel = this.getView().getModel();
+
+            MessageBox.confirm("Are you sure you want to delete Order " + oData.ProductID + "?", {
+                onClose: function (sAction) {
+                    if (sAction === MessageBox.Action.OK) {
+                        oModel.remove("/Orders(" + oData.ProductID + ")", {
+                            success: function () {
+                                MessageBox.success("Order deleted successfully!");
+                            },
+                            error: function () {
+                                MessageBox.error("Error deleting the order.");
+                            }
+                        });
+                    }
+                }
+            });
         }
 
 
